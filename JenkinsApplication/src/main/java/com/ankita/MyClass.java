3539cf541a35354c07bc3bba5b69229cbc685f8f
Ankita Taware <ankitataware941@gmail.com>
2019-12-31 09:31:46 +0530

    Committed!!

---
+++ JenkinsApplication/src/main/java/com/ankita/MyClass.java
@@ -0,0 +1,13 @@
+package com.ankita;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		int a=10;
+		int b=20;
+		System.out.println("Addition"+a+b);
+	}
+
+}
